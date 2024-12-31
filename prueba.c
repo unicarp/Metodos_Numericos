08b59fee2de1304a0b77551fd7f55a8fa6293f31
unicarp <jdomingueze04@mail.com>
2024-12-30 22:11:40 -0600

    Solucionando print de B

--- prueba.c
+++ prueba.c
@@ -2,16 +2,57 @@
 #include <stdio.h>
 #include <math.h>
 
+void manejoError(const char *mensaje){
+    fprintf(stderr,"Error: %s",mensaje);
+    exit(EXIT_FAILURE);//Indica el final del programa debido a un error
+}
+
+double *guardarArray(int n){
+    double *array = NULL;
+
+    array = (double *)malloc(sizeof(double)*n);
+    if(array == NULL){
+        manejoError("No se pudo asignar memoria correctamente.");
+    }
+
+    for(int i = 0; i <n; i++){
+        scanf("%lf", array + i);
+    }
+
+    return array;
+}
+
+void imprimirMatriz(double *array, int n, int m){   //n debe ser el numero de filas
+    printf("\n");                                   //m debe ser el numero de columnas
+    for(int i = 0; i <n; i++){
+        if(m > 1){
+            for(int j = 0; j < m; j++){
+                if(j == m-1){
+                    printf("%lf\n", *(array + (i*n) + j));
+                }else{
+                    printf("%lf ", *(array + (i*n) + j));
+                }
+            }
+        }else{
+            printf("%lf\n", *(array + i));
+        }
+    }
+}
+
 int main(){
-    double x = -10;
-    double y = 1.5;
-    double z = -1.5;
+    int n = 5; // Número de incógnitas
 
-    double xx = fabs(x);
+    double matriz[] = {
+	    1, 1, 1, 1, 7,         
+        0, 5, 2, 1, 2,  
+        0, 0, 7, 2, 2,  
+	    2, 1, 1, 0, 0,  
+        1, 2, 3, 10, 2   
+    };
+    
+    double *b = guardarArray(5);
 
-    double zz = fabs(z);
-
-    printf("%lf %lf",xx,zz);
+    imprimirMatriz(b,n,1);
 
     return 0;
 }
