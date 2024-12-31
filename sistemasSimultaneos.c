c92a99ca693ba6b9b8a838678988668ba3d1941f
unicarp <jdomingueze04@mail.com>
2024-12-30 19:10:18 -0600

    Diagonal de absolutos mayores

--- sistemasSimultaneos.c
+++ sistemasSimultaneos.c
@@ -18,14 +18,26 @@ double *guardarArray(int n){
     }
 
     for(int i = 0; i <n; i++){
-        scanf("%f", array + i);
+        scanf("%lf", array + i);
     }
 
     return array;
 }
 
+//Imprime la matriz de valores
+void imprimirMatriz(double *array, int n, int m){   //n debe ser el numero de columnas
+    printf("\n");                                   //m debe ser el numero de filas
+    for(int i = 0; i <n; i++){
+        if(i%(n/m) != (n/m)-1){
+            printf("%lff ", *(array + i));
+        }else{
+            printf("%lf\n", *(array + i));
+        }
+    }
+}
+
 //Valida si la matriz se puede resolver por este metodo
-double *validez(double *matriz,double *b, int n){//n debe ser el numero de incognitas
+void validez(double *matriz,double *b, int n){//n debe ser el numero de incognitas
     double *temporal = NULL;
 
     int m;          //numero para comprobar la posicion del absoluto mayor en la iteración atual
@@ -43,28 +55,30 @@ double *validez(double *matriz,double *b, int n){//n debe ser el numero de incog
     if(temporal == NULL){
         manejoError("No se pudo asignar memoria correctamente");
     }
+    
+    imprimirMatriz(matriz, n*n,n);
+    imprimirMatriz(b, n, 1);
+    fflush(stdout);
 
     while(i < n){//seguirá intentando acomodar las filas hasta que pueda verlas todas o hasta que encuentre uno con el absoluto mayor en la misma posición
         c = 0;
         r = 0;
-        j = i*n;
-        do{
-            if(j%n == 0){
-                absact = fabs(*(matriz + j));
+        for(j = 0; j < n; j++){
+            if(j == 0){
+                absact = fabs(*(matriz + (i*n)));
                 absmay = absact;
                 r = 0;
                 m = 0;
             }else{
                 absant = absact;
-                absact = fabs(*(matriz + j));
+                absact = fabs(*(matriz + (i*n) + j));
                 r++;                    
                 if(absmay < absact){
                     m = r;
                     absmay = absact;
                 }
             }
-            j++;
-        }while(j%n < n-1);
+        }
 
         if(m != i){
             if(p == m){
@@ -73,16 +87,29 @@ double *validez(double *matriz,double *b, int n){//n debe ser el numero de incog
             p = m;
             for(int k = 0; k <n; k++){
                 *(temporal + k) = *(matriz+(m*n)+k);
-                *(matriz+((m-1)*n)+k) = *(matriz+(i*n)+k);
+                *(matriz+(m*n)+k) = *(matriz+(i*n)+k);
                 *(matriz+(i*n)+k) = *(temporal + k);        //intercambiamos las filas de la matriz
             }
             *temporal = *(b+m);
             *(b+m) = *(b+i);
             *(b+i) = *(temporal);                           //intercambiamos las filas del vector b
+            imprimirMatriz(matriz, n*n,n);
+            imprimirMatriz(b, n, 1);
+            fflush(stdout);
         }else{
             i++;
         }
     }
+    imprimirMatriz(matriz, n,n);
+    imprimirMatriz(b, n, 1);
+    fflush(stdout);
+
+    for(i = 0; i < n; i++){
+
+    }
+
+    free(temporal);
+    temporal = NULL;
 }
 
 //Pivotea para garantizar la resolucion
@@ -114,20 +141,9 @@ double *gaussSeidel(int n, int iter,double error, double *matriz, double *x){
     return resultado;
 }
 
-//Imprime la matriz de valores
-void imprimirMatriz(double *array, int n, int m){
-    printf("\n");
-    for(int i = 0; i <n; i++){
-        if(i%(n/m) != (n/m)-1){
-            printf("%f ", *(array + i));
-        }else{
-            printf("%f\n", *(array + i));
-        }
-    }
-}
 
 int main(){
-    //Por ingresar algo
+    //placeholder
 
     return 0;
 }
